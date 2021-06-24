799f948ef8d3e64ce1e03b604fd707f13b229120
Konstantin Kuzminov <zal1358@gmail.com>
2021-06-24 16:07:11 +0200

    calendar working

--- lib/screens/events_calendar.dart
+++ lib/screens/events_calendar.dart
@@ -1,82 +1,94 @@
 import 'package:flutter/material.dart';
-
-import '../controllers/main_page_controller.dart';
-
 import 'package:get/get.dart';
-
 import 'package:table_calendar/table_calendar.dart';
 
+import '../controllers/events_storage.dart';
+import '../models/event.dart';
 import '../services/constants.dart';
+import '../widgets/event_card.dart';
 
-// TODO: переписать без стейта на GetX
+class EventsCalendar extends StatelessWidget {
+  final EventCalendarController c = Get.put(EventCalendarController());
 
-class EventsCalendar extends StatefulWidget {
-  @override
-  _EventsCalendarState createState() => _EventsCalendarState();
-}
-
-class _EventsCalendarState extends State<EventsCalendar> {
-  final MainPageController mainPageC = Get.find();
-
-  CalendarFormat _calendarFormat = CalendarFormat.month;
-
-  DateTime _focusedDay = DateTime.now();
-
-  DateTime? _selectedDay;
+  final EventsStorage eventsStorage = Get.find();
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TableCalendar(
-          // * в дальнейших портах на другие языки привязать к локали
-          availableCalendarFormats: const {
-            CalendarFormat.month: 'Месяц',
-            CalendarFormat.twoWeeks: '2 недели',
-            CalendarFormat.week: 'Неделя',
-          },
-          locale: "RU_ru",
-          focusedDay: _focusedDay,
-          firstDay: kFirstDay,
-          lastDay: kLastDay,
-          calendarFormat: _calendarFormat,
-          calendarStyle: CalendarStyle(
-            // Use `CalendarStyle` to customize the UI
-            outsideDaysVisible: false,
-            canMarkersOverflow: true,
+    c.setEvents(eventsStorage.getEventsByDay(DateTime.now()));
+
+    return Obx(() {
+      return Scrollbar(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              TableCalendar(
+                // * в дальнейших портах на другие языки привязать к локали
+                availableCalendarFormats: const {
+                  CalendarFormat.month: 'Месяц',
+                  CalendarFormat.twoWeeks: '2 недели',
+                  CalendarFormat.week: 'Неделя',
+                },
+                locale: "RU_ru",
+                focusedDay: c.focusedDay.value,
+                firstDay: kFirstDay,
+                lastDay: kLastDay,
+                calendarFormat: c.calendarFormat.value,
+                calendarStyle: CalendarStyle(
+                  // Use `CalendarStyle` to customize the UI
+                  outsideDaysVisible: false,
+                  canMarkersOverflow: true,
+                ),
+                // * выделение нужного дня
+                selectedDayPredicate: (day) {
+                  return isSameDay(c.selectedDay.value, day);
+                },
+                // Список для карточек событий по выбранному дню - формирование
+                onDaySelected: (selectedDay, focusedDay) {
+                  if (!isSameDay(c.selectedDay.value, selectedDay)) {
+                    c.setSelectedDay(selectedDay);
+                    c.setFocusedDay(focusedDay);
+                  }
+                  c.setEvents(eventsStorage.getEventsByDay(focusedDay));
+                },
+                // * смена формата календаря
+                onFormatChanged: (format) {
+                  c.setCalendarFormat(format);
+                },
+                // * сменя месяца (недели) - выбрать первое число, пересчитать события
+                onPageChanged: (focusedDay) {
+                  c.setFocusedDay(focusedDay);
+                  c.setSelectedDay(focusedDay);
+                  c.setEvents(eventsStorage.getEventsByDay(focusedDay));
+                },
+                // * сколько событий в данный день - отобразить точками
+                eventLoader: (DateTime day) {
+                  return eventsStorage.getEventsIdsByDay(day);
+                },
+              ),
+              for (Event e in c.eventsThisDay) EventCard(event: e),
+            ],
           ),
-          selectedDayPredicate: (day) {
-            return isSameDay(_selectedDay, day);
-          },
-          // TODO: выводить карточки событий по выбранному дню
-          onDaySelected: (selectedDay, focusedDay) {
-            if (!isSameDay(_selectedDay, selectedDay)) {
-              // Call `setState()` when updating the selected day
-              setState(() {
-                _selectedDay = selectedDay;
-                _focusedDay = focusedDay;
-              });
-            }
-          },
-          onFormatChanged: (format) {
-            setState(() {
-              _calendarFormat = format;
-            });
-          },
-          onPageChanged: (focusedDay) {
-            print(focusedDay);
-            _focusedDay = focusedDay;
-          },
-          // TODO: возвращать для каждого дня +- 3 года набор событий. Смотреть производительность.
-          eventLoader: (DateTime day) {
-            if (isSameDay(day, DateTime(kNow.year, kNow.month, kNow.day - 2))) {
-              return [1, 2, 3];
-            } else {
-              return [1, 2];
-            }
-          },
         ),
-      ],
-    );
+      );
+    });
   }
+}
+
+class EventCalendarController extends GetxController {
+  var calendarFormat = CalendarFormat.month.obs;
+
+  setCalendarFormat(CalendarFormat newFormat) =>
+      calendarFormat.value = newFormat;
+
+  var focusedDay = DateTime.now().obs;
+
+  setFocusedDay(DateTime newDay) => focusedDay.value = newDay;
+
+  var selectedDay = DateTime.now().obs;
+
+  setSelectedDay(DateTime newDay) => selectedDay.value = newDay;
+
+  var eventsThisDay = [].obs;
+
+  setEvents(List<Event> events) => eventsThisDay.value = events;
 }
